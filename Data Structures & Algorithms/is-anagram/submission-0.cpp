01cb9e5a70dcb9dad25f818915df9ef116e006d9
Shreyash CHaurasia <55597451+shreyash1611@users.noreply.github.com>
2026-08-10 08:18:14 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // valid anagram
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());  
+        return t==s;
+    }
+};
